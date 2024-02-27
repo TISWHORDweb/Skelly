44955fd0547a8a857bb35e3faa3b4f6e596a58e1
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-02-27 15:58:36 +0100

    Customer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,6 +4,8 @@ import Banner from './Banner'
 import Features from './Features'
 import Experience from './Experience'
 import Discover from './Discover'
+import Customers from '../../Components/Customers'
+import ContactCard from '../../Components/ContactCard'
 
 function Home() {
   return (
@@ -13,6 +15,8 @@ function Home() {
         <Features />
         <Experience />
         <Discover />
+        <Customers />
+        <ContactCard />
     </div>
   )
 }
